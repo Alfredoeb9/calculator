e99f0b522e1015f79aa37a91cf50e363d3426ae4
alfredo <you@example.com>
2019-07-15 09:12:13 -0700

    deleted extra function

--- main.js
+++ main.js
@@ -14,60 +14,10 @@ function calc() {
     calculate = (a / b);
   } else if (op === "mul") {
     calculate = (a * b);
-  };
+  }
 
   const final = document.querySelector('#result').innerHTML = calculate;
   // document.querySelector('#result').innerHTML = calculate;
   return final;
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function calc2() {
-  let a = parseInt(document.querySelector('#value1').value);
-  let b = parseInt(document.querySelector('#value2').value);
-  let op = document.querySelector('#operator').value;
-
-  let calculate;
-
-  if (op == "add") {
-    calculate = (a + b);
-  } else if (op == "sub") {
-    calculate = (a - b);
-  } else if (op == "div") {
-    calculate = (a / b)
-  } else if (op == "mul") {
-    calculate = (a * b);
-  };
-
-  const final = document.querySelector('#result').innerHTML = calculate
-  // document.querySelector('#result').innerHTML = calculate;
-  return final;
 }
